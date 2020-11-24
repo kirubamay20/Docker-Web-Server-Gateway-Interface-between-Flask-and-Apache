cc23212254b07d0cd04b947c06a4c187dd719e7d
KIRUBA KARUPPANNAN <61502293+kirubamay20@users.noreply.github.com>
2020-11-24 21:38:42 +0530

    Create flask_predict.py

---
+++ flask_predict.py
@@ -0,0 +1,19 @@
+import pickle 
+from flask import Flask,request
+from flasgger import Swagger
+import pandas as pd
+import numpy as np
+
+with open('*/breast_cancer_rf.pkl', 'rb') as model_file:
+  model=pickle.load(model_file)
+
+app1=Flask(__name__)
+swagger=Swagger(app1)
+
+@app.route('/predict')
+def predict_cancer_file():
+    input_data = pd.read_csv(request.files.get["input_file"], header=None)
+    prediction = model.predict(input_data)
+    return str(list(prediction))
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=5000)
